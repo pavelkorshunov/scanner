1af1e356dd9a815f4b3c259053039badac0c1454
Pavel <korsun4@mail.ru>
2022-12-05 16:32:05 +0300

    red file

--- scanner.go
+++ scanner.go
@@ -5,7 +5,21 @@ import (
 	"os"
 )
 
+var scannerFiles = []string{
+	"./test.txt",
+}
+
 func main() {
-	args := os.Args
+	args := os.Args[1:]
 	fmt.Println(args)
+
+	for _, val := range scannerFiles {
+		content, err := os.ReadFile(val)
+		if err != nil {
+			fmt.Println("error read file")
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", content)
+	}
 }
